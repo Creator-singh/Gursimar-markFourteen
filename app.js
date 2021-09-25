ee47c9c8bd19b2752a37bcc7401a5ef31b5f18d6
Creator-singh <64949453+Creator-singh@users.noreply.github.com>
2021-09-25 15:16:09 +0530

    updated app.js and logic

--- app.js
+++ app.js
@@ -4,11 +4,39 @@ const currentPrice = document.querySelector("#current-price");
 const submit = document.querySelector("#submit");
 const output = document.querySelector("#output");
 
-submit.addEventListener("click", function calculateProfitLoss(){
+
+function calculateProfitLoss(initial, qty, current)
+{
+    if(initial < current)
+    {
+        //profit
+        var profit = (current - initial) * qty;
+        var profitPercentage = (profit/(initial * qty)) * 100;
+        output.style.color = '#1b998b';
+        output.innerText = `Yay! your Profit is ${profit} and Profit Percentage is ${profitPercentage.toFixed(2)}% :)`;
+    }
+    else if(initial > current)
+    {
+        //loss
+        var loss = (initial - current) * qty;
+        var lossPercentage = (loss/(initial * qty)) * 100;
+        output.style.color = '#bb4430';
+        output.innerText = `Oops! your Loss is ${loss} and Loss Percentage is ${lossPercentage.toFixed(2)}% :(`;
+    }
+    else{
+        //net zero
+        output.style.color = 'black';
+        output.innerText = " No Profit/Loss 😪";
+    }
+}
+
+submit.addEventListener("click", function submitHandler(){
     if(Number(initialPrice.value) > 0 && Number(quantity.value) > 0 && Number(currentPrice.value) > 0)
     {
-        output.style.color = '#1b998b';
-        output.innerText = "everything ok";
+        var initial = Number(initialPrice.value);
+        var qty = Number(quantity.value);
+        var current = Number(currentPrice.value);
+        calculateProfitLoss(initial, qty, current);
     }
     else{
         output.style.color = '#bb4430';
